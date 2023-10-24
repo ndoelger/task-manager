4d845199ded8a0159494f0fa3f1c4b10028d15c5
ndoelger <doelgern@gmail.com>
2023-10-24 19:45:36 -0400

    radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+    <Button>New Issue</Button>
   );
 }
